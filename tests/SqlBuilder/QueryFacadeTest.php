5cbcc6333fb02d0dc4ea93eb4398089d480ade77
Alexey Seleznev <aleksey.seleznev@softgamings.com>
2022-04-29 21:30:33 +0300

    test `Join` for `QueryFacade`

--- tests/SqlBuilder/QueryFacadeTest.php
+++ tests/SqlBuilder/QueryFacadeTest.php
@@ -83,6 +83,31 @@ class QueryFacadeTest extends TestCase
     }
 
     /**
+     * Test JOIN
+     */
+    public function testJoin(): void
+    {
+        $ordersSubqueryFacade = new QueryFacade(new Query("Orders"));
+        $ordersSubqueryFacade->where("date BETWEEN '2021-01-01' AND '2021-01-31'");
+
+        $facade = new QueryFacade(new Query("Users", "U"));
+        $facade->select([
+            "U.Login",
+            ["D.Name", "Department"],
+            ["R.Name", "Role"],
+            ["O.ID", "OrderID"]
+        ])
+            ->leftJoin("Departments", "D", new ConditionStmt('U.DepartmentID', '=', 'D.ID'))
+            ->rightJoin(new Query("Roles"), "R", 'U.RoleID = R.ID')
+            ->innerJoin($ordersSubqueryFacade, "O", 'U.ID = O.UserID');
+
+        $this->assertSame(
+            "SELECT U.Login, D.Name AS Department, R.Name AS Role, O.ID AS OrderID FROM Users AS U LEFT JOIN Departments AS D ON ((U.DepartmentID = D.ID)) RIGHT JOIN (SELECT * FROM Roles) AS R ON (U.RoleID = R.ID) INNER JOIN (SELECT * FROM Orders WHERE date BETWEEN '2021-01-01' AND '2021-01-31') AS O ON (U.ID = O.UserID)",
+            $facade->buildSql()
+        );
+    }
+
+    /**
      * Test WHERE
      */
     public function testWhere(): void
